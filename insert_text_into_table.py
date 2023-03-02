c0081f81467ee0f4fb038994c1a62d94a4cf8568
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-03-02 01:36:50 +0000

    Update insert_text_into_table.py

--- insert_text_into_table.py
+++ insert_text_into_table.py
@@ -9,10 +9,10 @@ def split_text_into_lines(text):
     return lines
 
 
-def insert_text_into_table(text,tag):
+def insert_text_into_table(text,tag,base,table):
     blocks = split_text_into_lines(text)
     for block in blocks:
       try:
-        insert_record("base_blocks","INSERT INTO table_blocks VALUES (NULL,'"+tag+"','"+block+"')")
+        insert_record(base,"INSERT INTO "+base+" VALUES (NULL,'"+tag+"','"+block+"')")
       except:
         print("Error inserting record")
